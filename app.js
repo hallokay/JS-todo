534ab70f2465e13afa382338f059cf49aa4b6cd0
Kay <69727381+hallokay@users.noreply.github.com>
2021-05-25 18:26:36 +0900

    05.25

--- app.js
+++ app.js
@@ -3,6 +3,31 @@ const todoInput = document.querySelector('.todo_input'),
     todoList = document.querySelector('.todo_list');
 
 
+let todos;
+todos = [];
+
+
+// SAVE TODO
+function saveTodo(todo) {
+    if(localStorage.getItem('todos') !== null) {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+
+    todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+// get todo
+function getTodo(){
+    if(localStorage.getItem('todos') !== null) {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+
+    todos.forEach( todo => {
+
+    })
+}
+
 //ADD TODO
 
 function addTodo(e) {
@@ -15,17 +40,18 @@ function addTodo(e) {
     // todo li
     const todoLi = document.createElement('li');
     todoLi.classList.add('todo_li');
-    todoLi.innerText = '엉덩qndqnsqndqnd';
+    todoLi.innerText = todoInput.value;
     //텍스트만 들어감
+    todoInput.value = '';
 
     // todo button
     const completeBtn = document.createElement('button');
-    completeBtn.classList.add('todo_li_btn');
+    completeBtn.classList.add('check_btn');
     completeBtn.innerHTML = '<i class="fas fa-check"></i>';
     // html태그 요소를 넣고 싶을 때는 innerHTML을 쓴다
 
     const trashBtn = document.createElement('button');
-    trashBtn.classList.add('todo_li_btn');
+    trashBtn.classList.add('trash_btn');
     trashBtn.innerHTML = '<i class="fa fa-trash"></i>';
 
 
@@ -35,8 +61,70 @@ function addTodo(e) {
 
     todoList.appendChild(todoDiv);
 
-
+    // SAVE TODO
+    saveTodo(todoInput.value);
 }
 
-
 todoBtn.addEventListener('click', addTodo);
+
+
+
+function deleteCheck(e) {
+    const item = e.target;
+    const todo = item.parentElement;
+
+
+    // =======delete todo======
+    // 아이템의 클래스의 첫번째 인덱스가 삭제이면
+    if(item.classList[0] === 'trash_btn') {
+        todo.classList.add('todo_fall');
+        todo.addEventListener('transitionend', () => {
+            todo.remove();
+        });
+
+    }
+
+    // ==========check todo=========
+    if(item.classList[0] === 'check_btn') {
+        todo.classList.toggle('todo_completed');
+    }
+
+};
+
+todoList.addEventListener('click', deleteCheck);
+
+
+
+// ===== filter option====
+const filterOption = document.querySelector('.filter_todo');
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach( todo => {
+        switch(e.target.value) {
+            case "all":
+                todo.style.display = "flex";
+                break;
+
+            case "completed":
+                if(todo.classList.contains('todo_completed')) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+            case "uncompleted":
+                if(!todo.classList.contains('todo_completed')) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+    
+        }
+    })
+
+}
+filterOption.addEventListener('click', filterTodo);
+
+
